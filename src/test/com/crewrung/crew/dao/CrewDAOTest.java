a3aac9890cd114b793afcd8e55e09ed29a63b445
dehetnim <jng3268@gmail.com>
2025-04-24 14:09:52 +0900

    feat: CrewDAOTest.java 테스트 완료

--- src/test/com/crewrung/crew/dao/CrewDAOTest.java
+++ src/test/com/crewrung/crew/dao/CrewDAOTest.java
@@ -59,14 +59,11 @@ public class CrewDAOTest {
 //	public void successSetCrewMeeting() {
 //		assertTrue(dao.setCrewMeeting(new CrewMeetingVO("", "새 제목", "새 내용", "2025-01-11", 6, 1111000000, 2)));
 //	}
-
 //
 //	@Test
 //	public void failSetCrewMeeting() {
 //		try {
-
 //			dao.setCrewMeeting(new CrewMeetingVO("", "새 제목", "새 내용", "2025-12-11", 6, 1000000, 2));
-
 //		} catch (Exception e) {
 //			assertTrue(e.getClass() == PersistenceException.class);
 //		}
@@ -176,31 +173,31 @@ public class CrewDAOTest {
 //	}
 	
 	@Test //홍보하는 크루정보 조회(기댓값이 옳지 않은 경우)
-	public void 홍보크루정보조회테스트1() {
+	public void 홍보크루정보조회테스트실패() {
 		List<PromotionVO> result = dao.getAllPromotionCrew();
 		assertNotEquals(7, result.size());
 	}
 
 	@Test //홍보하는 크루정보 조회(기댓값이 옳은 경우)
-	public void 홍보크루정보조회테스트2() {
+	public void 홍보크루정보조회테스트성공() {
 		List<PromotionVO> result = dao.getAllPromotionCrew();
 		assertEquals(6, result.size());
 	}
 
 	@Test //모든 크루정보 조회(기댓값이 옳지 않은 경우)
-	public void 모든크루정보조회테스트1() {
+	public void 모든크루정보조회테스트실패() {
 		List<AllCrewVO> result = dao.getAllCrew();
 		assertNotEquals(9, result.size());
 	}
 
 	@Test //모든 크루정보 조회(기댓값이 옳은 경우)
-	public void 모든크루정보조회테스트2() {
+	public void 모든크루정보조회테스트성공() {
 		List<AllCrewVO> result = dao.getAllCrew();
 		assertNotEquals(10, result.size());
 	}
 
 	@Test //필터 적용된 모든 크루정보 조회(기댓값이 옳지 않은 경우)
-	public void 필터적용된모든크루정보조회테스트1() {
+	public void 필터적용된모든크루정보조회테스트실패() {
 		Map<String, String> filter = new HashMap<>();
 		filter.put("interestCategory", null);
 		filter.put("guName", "마포구");
@@ -212,7 +209,7 @@ public class CrewDAOTest {
 	}
 
 	@Test //필터 적용된 모든 크루정보 조회(기댓값이 옳은 경우)
-	public void 필터적용된모든크루정보조회테스트2() {
+	public void 필터적용된모든크루정보조회테스트성공() {
 		Map<String, String> filter = new HashMap<>();
 		filter.put("interestCategory", "운동");
 		filter.put("guName", "송파구");
@@ -223,73 +220,73 @@ public class CrewDAOTest {
 	}
 
 	@Test //크루 상세정보 조회(기댓값이 옳지 않은 경우)
-	public void 크루상세정보조회테스트1() {
+	public void 크루상세정보조회테스트실패() {
 		AllCrewVO result = dao.getCrewDetail(7);
 		assertNotEquals("슬램덩크", result.getCrewName());
 	}
 
 	@Test //크루 상세정보 조회(기댓값이 옳은 경우)
-	public void 크루상세정보조회테스트2() {
+	public void 크루상세정보조회테스트성공() {
 		AllCrewVO result = dao.getCrewDetail(7);
 		assertEquals("북적북적", result.getCrewName());
 	}
 
 	@Test //크루장 정보 조회(기댓값이 옳지 않은 경우)
-	public void 크루장정보조회테스트1() {
+	public void 크루장정보조회테스트실패() {
 		CrewLeaderVO result = dao.getCrewLeader(7);
 		assertNotEquals("이상혁", result.getName());
 	}
 
 	@Test //크루장 정보 조회(기댓값이 옳은 경우)
-	public void 크루장정보조회테스트2() {
+	public void 크루장정보조회테스트성공() {
 		CrewLeaderVO result = dao.getCrewLeader(7);
 		assertEquals("이우진", result.getName());
 	}
 
 	@Test //크루 생성하기(기댓값이 옳지 않은 경우)
-	public void 크루생성테스트1() {
+	public void 크루생성테스트실패() {
 		int result = 0;
 		result = dao.addCrew(new CrewVO("북적북적", "kyeongmin56", "함께 모여 북적북적 활동해요!", "운동", "20대-30대", "crew1.jpg", "지금 바로 참여하세요!", 'Y', 1111000000));
 		assertNotEquals(result, 1);
 	}
 
 	@Test //크루 생성하기(기댓값이 옳은 경우)
-	public void 크루생성테스트2() {
+	public void 크루생성테스트성공() {
 		int result = 0;
 		result = dao.addCrew(new CrewVO("북적북작", "kyeongmin56", "함께 모여 북적북적 활동해요!", "운동", "20대-30대", "crew11jpg", "지금 바로 참여하세요!", 'Y', 1111000000));
 		assertEquals(result, 1);
 	}
 
 	@Test //크루 수정하기(기댓값이 옳지 않은 경우)
-	public void 크루수정테스트1() {
+	public void 크루수정테스트실패() {
 		int result = 0;
 		result = dao.updateCrew(new CrewVO(11, "북적북적", "kyeongmin56", "함께 모여 북적북적 활동해요!", "운동", "20대-30대", "crew1.jpg", "지금 바로 참여하세요!", 'Y', 1111000000));
 		assertNotEquals(result, 0);
 	}
 
 	@Test //크루 수정하기(기댓값이 옳은 경우)
-	public void 크루수정테스트2() {
+	public void 크루수정테스트성공() {
 		int result = 0;
 		result = dao.updateCrew(new CrewVO(7, "북적북적", "kyeongmin56", "함께 모여 북적북적 활동해요!", "운동", "20대-30대", "crew1.jpg", "지금 바로 참여하세요!", 'Y', 1111000000));
 		assertEquals(result, 1);
 	}
 
 	@Test //크루 전체 댓글 개수 조회(기댓값이 옳지 않은 경우)
-	public void 크루댓글개수조회테스트1() {
+	public void 크루댓글개수조회테스트실패() {
 		int result = 0;
 		result = dao.getCommentCount(5);
 		assertNotEquals(result, 1);
 	}
 
 	@Test //크루 전체 댓글 개수 조회(기댓값이 옳은 경우)
-	public void 크루댓글개수조회테스트2() {
+	public void 크루댓글개수조회테스트성공() {
 		int result = 0;
 		result = dao.getCommentCount(5);
 		assertEquals(result, 2);
 	}
 
 	@Test //크루 댓글 작성자정보 및 댓글 내용 조회(기댓값이 옳지 않은 경우)
-	public void 크루댓글내용조회테스트1() {
+	public void 크루댓글내용조회테스트실패() {
 		List<CrewCommentVO> result = new ArrayList<>();
 		result = dao.getCrewCommentDetail(9);
 		boolean found = result.stream()
@@ -298,7 +295,7 @@ public class CrewDAOTest {
 	}
 
 	@Test //크루 댓글 작성자정보 및 댓글 내용 조회(기댓값이 옳은 경우)
-	public void 크루댓글내용조회테스트2() {
+	public void 크루댓글내용조회테스트성공() {
 		List<CrewCommentVO> result = new ArrayList<>();
 		result = dao.getCrewCommentDetail(9);
 		boolean found = result.stream()
@@ -307,21 +304,21 @@ public class CrewDAOTest {
 	}
 
 	@Test //크루 가입 신청(기댓값이 옳지 않은 경우)
-	public void 크루가입신청테스트1() {
+	public void 크루가입신청테스트실패() {
 		int result = 0;
 		result = dao.applyToCrew(new CrewApplicationVO(8, "cunjabag4556", "운동 좋아요. 함께 해요!"));
 		assertNotEquals(result, 1);
 	}
 
 	@Test //크루 가입 신청(기댓값이 옳은 경우)
-	public void 크루가입신청테스트2() {
+	public void 크루가입신청테스트성공() {
 		int result = 0;
 		result = dao.applyToCrew(new CrewApplicationVO(8, "cunjabag45", "운동 좋아요. 함께 해요!"));
 		assertEquals(result, 1);
 	}
 
 	@Test //크루 관리페이지 조회(크루가입신청자)(기댓값이 옳지 않은 경우)
-	public void 크루관리페이지의크루가입신청자조회테스트1() {
+	public void 크루관리페이지의크루가입신청자조회테스트실패() {
 		List<CrewManagePageVO> result = new ArrayList<>();
 		result = dao.getCrewApplicants(1);
 		boolean found = result.stream()
@@ -330,7 +327,7 @@ public class CrewDAOTest {
 	}
 
 	@Test //크루 관리페이지 조회(크루가입신청자)(기댓값이 옳은 경우)
-	public void 크루관리페이지의크루가입신청자조회테스트2() {
+	public void 크루관리페이지의크루가입신청자조회테스트성공() {
 		List<CrewManagePageVO> result = new ArrayList<>();
 		result = dao.getCrewApplicants(1);
 		boolean found = result.stream()
@@ -339,7 +336,7 @@ public class CrewDAOTest {
 	}
 
 	@Test //크루 관리페이지 조회(크루원)(기댓값이 옳지 않은 경우)
-	public void 크루관리페이지의크루원조회테스트1() {
+	public void 크루관리페이지의크루원조회테스트실패() {
 		List<CrewManagePageVO> result = new ArrayList<>();
 		result = dao.getCrewMember(2);
 		boolean found = result.stream()
@@ -348,7 +345,7 @@ public class CrewDAOTest {
 	}
 
 	@Test //크루 관리페이지 조회(크루원)(기댓값이 옳은 경우)
-	public void 크루관리페이지의크루원조회테스트2() {
+	public void 크루관리페이지의크루원조회테스트성공() {
 		List<CrewManagePageVO> result = new ArrayList<>();
 		result = dao.getCrewMember(2);
 		boolean found = result.stream()
