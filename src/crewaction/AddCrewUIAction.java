b0b26df440c668a21d5bf2a9d5afecdac1b2780f
dehetnim <jng3268@gmail.com>
2025-04-25 15:25:54 +0900

    feat: CrewAction 1차 추가

---
+++ src/crewaction/AddCrewUIAction.java
@@ -0,0 +1,19 @@
+package crewaction;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.crewrung.servlet.Action;
+
+public class AddCrewUIAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+
+		return "addCrew.jsp";
+	}
+}
+
+
