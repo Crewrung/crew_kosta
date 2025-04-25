0c0f93ff1e5bae57bd2ebe595dc3f131ef45ed10
kokoko12334 <gnjgnjgnj1@gmail.com>
2025-04-25 17:50:20 +0900

    feat: servlet FrontController action들작성

--- src/com/crewrung/servlet/FrontController.java
+++ src/com/crewrung/servlet/FrontController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/")
+@WebServlet("/controller")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
