6f503eee252cd8c8c3bff91af8915470e26f61ef
dehetnim <jng3268@gmail.com>
2025-04-25 18:02:00 +0900

    feat:크루 action 추가

---
+++ src/com/crewrung/crew/action/AddCrewMeetingUIAction.java
@@ -0,0 +1,18 @@
+package com.crewrung.crew.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.crewrung.servlet.Action;
+
+public class AddCrewMeetingUIAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		
+		return "addMeeting.jsp";
+	}
+
+}
---
+++ src/com/crewrung/crew/action/AddCrewUIAction.java
@@ -0,0 +1,19 @@
+package com.crewrung.crew.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.crewrung.servlet.Action;
+
+public class AddCrewUIAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+
+		return "addCrew.jsp";
+	}
+}
+
+
