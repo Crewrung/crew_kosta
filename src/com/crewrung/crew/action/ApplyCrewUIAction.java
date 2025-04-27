ab69a243ed25e9a0448f0c6f25e08b4fd9a817d3
dehetnim <jng3268@gmail.com>
2025-04-27 14:33:50 +0900

    feat: ApplyCrewUIAction.java 추가

---
+++ src/com/crewrung/crew/action/ApplyCrewUIAction.java
@@ -0,0 +1,17 @@
+package com.crewrung.crew.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import com.crewrung.servlet.Action;
+
+public class ApplyCrewUIAction implements Action {
+	
+	@Override
+	public String execute(HttpServletRequest request) throws ServletException, IOException {
+		
+		return "crewApplyPage.html";
+	}
+}
