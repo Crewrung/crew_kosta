9393d6302e5cfa26e736eed7f403aeb8d1dcc69d
taebin <xoqls5824@gmail.com>
2025-04-28 14:29:08 +0900

    feat

--- src/com/crewrung/crew/action/AddCrewMeetingAction.java
+++ src/com/crewrung/crew/action/AddCrewMeetingAction.java
@@ -17,23 +17,24 @@ public class AddCrewMeetingAction implements Action {
 
     @Override
     public String execute(HttpServletRequest request) throws ServletException, IOException {
-        CrewService service = new CrewService(new CrewDAO(MyBatisUtil.getSqlSession()));
-
-        CrewMeetingVO meetingVO = new CrewMeetingVO();
-        meetingVO.setTitle(request.getParameter("title"));
-        meetingVO.setContent(request.getParameter("content"));
-        meetingVO.setMeetingDate(request.getParameter("meetingDate")); // 날짜와 시간 통합
-        meetingVO.setMaxMember(Integer.parseInt(request.getParameter("maxMember")));
-        meetingVO.setCrewNumber(Integer.parseInt(request.getParameter("crewNumber")));
-        meetingVO.setCrewMeetingHostNumber(Integer.parseInt(request.getParameter("crewMeetingHostNumber")));
-        meetingVO.setGuNumber(Integer.parseInt(request.getParameter("guNumber")));
-
-        boolean result = service.addCrewMeetingService(meetingVO);
-
-        if (result > 0) {
-            return "crew/crewAddMeetingResult.jsp?message=크루 모임이 성공적으로 생성되었습니다.&redirectUrl=/controller?cmd=crewUI";
-        } else {
-            return "crew/crewAddMeetingResult.jsp?message=크루 모임 생성에 실패했습니다.&redirectUrl=/crew/crewMeetingAddPage.html";
-        }
+//        CrewService service = new CrewService(new CrewDAO(MyBatisUtil.getSqlSession()));
+//
+//        CrewMeetingVO meetingVO = new CrewMeetingVO();
+//        meetingVO.setTitle(request.getParameter("title"));
+//        meetingVO.setContent(request.getParameter("content"));
+//        meetingVO.setMeetingDate(request.getParameter("meetingDate")); // 날짜와 시간 통합
+//        meetingVO.setMaxMember(Integer.parseInt(request.getParameter("maxMember")));
+//        meetingVO.setCrewNumber(Integer.parseInt(request.getParameter("crewNumber")));
+//        meetingVO.setCrewMeetingHostNumber(Integer.parseInt(request.getParameter("crewMeetingHostNumber")));
+//        meetingVO.setGuNumber(Integer.parseInt(request.getParameter("guNumber")));
+//
+//        boolean result = service.addCrewMeetingService(meetingVO);
+//
+//        if (result > 0) {
+//            return "crew/crewAddMeetingResult.jsp?message=크루 모임이 성공적으로 생성되었습니다.&redirectUrl=/controller?cmd=crewUI";
+//        } else {
+//            return "crew/crewAddMeetingResult.jsp?message=크루 모임 생성에 실패했습니다.&redirectUrl=/crew/crewMeetingAddPage.html";
+//        }
+    	return null;
     }
 }
