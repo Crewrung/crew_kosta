179df4d95bf8224e2ba4034853e884affdffe942
taebin <xoqls5824@gmail.com>
2025-04-28 14:04:31 +0900

    마이페이지 구현

--- src/com/crewrung/account/action/MypageUIAction.java
+++ src/com/crewrung/account/action/MypageUIAction.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import com.crewrung.servlet.Action;
 
@@ -11,8 +12,12 @@ public class MypageUIAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		HttpSession session = request.getSession(false);
+		String userId = (String) session.getAttribute("userId");
+		if(session == null || userId == null){
+			request.setAttribute("errorMsg", "로그인 후 마이페이지에 접근 가능합니다.");
+			return "/account/login.jsp";
+		}
 		return "/account/mypage.jsp";
 	}
-
 }
